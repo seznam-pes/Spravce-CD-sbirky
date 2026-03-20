05ee6d20f7c1253cf0c5b7fbd76470aa69cc1216
seznam pes <frantisekburda@icloud.com>
2026-03-20 14:24:40 +0100

    Add files via upload

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -16,9 +16,34 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        Backend bejros = new Backend();
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = bejros;
+        }
+
+        public string nazev;
+        public string umelec;
+        public DateTime datumvydani;
+        public int songcount;
+        public string genre;
+        public string label;
+        public DateTime datumpridani;
+
+        public void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            string tempcount = pocet_skladeb.Text;
+
+            nazev = name.Text;
+            umelec = artist.Text;
+            datumvydani = releasedate.SelectedDate.Value.Date;
+            songcount = int.Parse(tempcount);
+            genre = zanr.Text;
+            label = vydavatelstvi.Text;
+            datumpridani = adddate.SelectedDate.Value.Date;
+            bejros.AddCD();
         }
     }
 }
